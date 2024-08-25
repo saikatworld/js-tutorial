af0f8df2d4b5193cfec13acd2bd7ac199ea1b965
saikat pandit <saikatpandit2424@gmail.com>
2024-08-25 12:20:02 +0530

    done

---
+++ .dist/comparisons.js
@@ -0,0 +1,30 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+// confusion ante hay 
+
+console.log(null > 0);//clean code likhna chahiye 
+console.log(null == 0);
+console.log(null >= 0);
+
+
+
+// console.log(undefined== 0);
+
+// === 
+
+console.log("3" === 3);
+
+
+
+
+
+
